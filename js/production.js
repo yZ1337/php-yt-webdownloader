8b91988343a7779a14d0d3b4fbbbda8eb5a2a0ca
tv33 <thomas.verhoeven3@hva.nl>
2023-02-26 22:04:55 +0100

    Update

--- js/production.js
+++
@@ -1,5 +0,0 @@
-// his file is made by: oVeXz
-// Github: https://github.com/oVeXz 
-// Feel free to use this, but please promote it when using it for your own projects!
-
-!function(){window;const e=document.documentElement;if(e.classList.remove("no-js"),e.classList.add("js"),document.body.classList.contains("has-animations")){(window.sr=ScrollReveal()).reveal(".reveal-on-scroll",{duration:600,distance:"20px",easing:"cubic-bezier(0.5, -0.01, 0, 1.005)",origin:"bottom",interval:100})}}();
